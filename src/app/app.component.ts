8dbb6bc8abfb432afec124ba5260f0a6fb0070fe
reynanwq <reynanwq@gmail.com>
2025-01-12 12:17:19 -0300

    feat: create test-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TesteComponent } from './teste/teste.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, TesteComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'caronaApp';
