158a5dd44c776593f028a2be4d23cd93c6bc8d71
reynanwq <reynanwq@gmail.com>
2025-01-14 18:17:10 -0300

    feat: recover password page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RegisterComponent],
+  imports: [
+    RouterOutlet,
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
